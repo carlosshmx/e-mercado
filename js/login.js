742d229de55df1a12185ec661b1e5a5824c12e7f
Carlos Colmenares <carlosshmx@hotmail.com>
2022-08-11 02:36:53 -0300

    actualizado el login

--- js/login.js
+++ js/login.js
@@ -1,24 +1,33 @@
-document.getElementById("login-button").addEventListener("click", showInputsErrors);
+document.addEventListener("DOMContentLoaded", ()=>{
+  document.querySelector("form").addEventListener("submit", (checkdata) =>{
+    checkdata.preventDefault();
+    let inputcontainers = document.querySelectorAll(".input-container");
+    let allwhite = true;
 
-function showInputsErrors() {
-		var inputs = document.querySelectorAll(".form-control");
-        console.log(inputs)
-        for(input of inputs){
-            if (input.value == '') {
-                input.style.border = "solid red 3px";
-            }
-            else {
-                input.style.border = "";
-            }
+    for(inputcont of inputcontainers){
+      let input = inputcont.querySelector(".form-control")
+        if (input.value == '') {
+            inputcont.querySelector("p").style.visibility = "visible";
+            input.style.border = "solid red 3px";
+            allwhite = false;
         }
-    
-}
+        else {
+            inputcont.querySelector("p").style.visibility = "hidden";
+            input.style.border = "";
+        }
+    }
+    if(allwhite){
+      window.location.replace("../home.html");
+    }
+  });
+  
+})
 
 function passwordShow(){
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
   }
+}
