05fef24067de07db65c3750dc10496436abb2ab1
Carlos Colmenares <carlosshmx@hotmail.com>
2022-08-19 11:02:41 -0300

    utilizada la funcion fetch principal

--- js/products.js
+++ js/products.js
@@ -1,34 +1,22 @@
-
-async function fetchdata(url){
-    try{
-        const response = await fetch(url);
-        const data = await response.json();
-        return data;
-    }catch(error){
-        console.log(error);
-    }
-}
-
+let currentProductArray = [];
 
 async function showProductList(){
 
-    const autos = await fetchdata("https://japceibal.github.io/emercado-api/cats_products/101.json");
-
-    let htmlContentToAppend = "";
-    for(auto of autos.products){
-
+        let htmlContentToAppend = "";
+        for(product of currentProductArray.products){
+        
         htmlContentToAppend += `
-            <div onclick="setAutoID(${auto.id})" class="list-group-item list-group-item-action cursor-active">
+            <div onclick="setAutoID(${product.id})" class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
-                        <img src="${auto.image}" alt="${auto.name}" class="img-thumbnail">
+                        <img src="${product.image}" alt="${product.name}" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${auto.name} - ${auto.currency} ${auto.cost}</h4>
-                            <small class="text-muted">${auto.soldCount} Vendidos</small>
+                            <h4 class="mb-1">${product.name} - ${product.currency} ${product.cost}</h4>
+                            <small class="text-muted">${product.soldCount} Vendidos</small>
                         </div>
-                        <p class="mb-1">${auto.description}</p>
+                        <p class="mb-1">${product.description}</p>
                     </div>
                 </div>
             </div>
@@ -38,7 +26,13 @@ async function showProductList(){
     document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
 }
 
+
 document.addEventListener("DOMContentLoaded", function(e){
-    showProductList();
-    }
-)
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){
+            currentProductArray = resultObj.data
+            showProductList()
+
+        }
+    });
+});
